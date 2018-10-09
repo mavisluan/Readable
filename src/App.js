fff2156240f0e9d107f8505b92ef0a58d249de92
Yi <mavisluan83@gmail.com>
2018-10-09 10:53:07 -0700

    add SideBar

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <div>
+        <div className='App'>
           <SideBar />
         </div>
       </Router>
--- src/SideBar.js
+++ src/SideBar.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { fetchCategories } from './actions/categories'
+import { Link } from 'react-router-dom'
 
 class SideBar extends Component {
     componentDidMount() {
@@ -11,12 +12,18 @@ class SideBar extends Component {
         const { categories } = this.props
 
         return (
-            <div>
-                {categories && categories.map(category => (
-                    <li key={category.path}>
-                        {category.name}
-                    </li>
+            <div className='side-bar'>
+                <Link to='/' >All categories</Link>
+                <ul>
+                    {categories && categories.map(category => (
+                        <li key={category.name} >
+                            <Link to={`/${category.name}`}>
+                                {category.name}
+                            </Link>
+                        </li>
                     ))}
+                </ul> 
+                    <Link to='/new'>New Post</Link>
             </div>
         )
     }
